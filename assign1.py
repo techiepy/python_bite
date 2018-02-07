f1cb5436413fa2adf0006e08170f2951da0cccb2
techiepy <aromal4frnz@gmail.com>
2018-02-07 16:36:10 +0530

    initial commit

---
+++ assign1.py
@@ -0,0 +1,8 @@
+
+Sessions_Attended = {'sessions' : '1011,2344,3222,44322,555,6332,721,8789,99,1011,1124,1245,137,1499'}
+
+x = Sessions_Attended.values()
+y = str(x).split(",")
+z = len(y)
+print("I have attended",z,"sessions!!")
+
